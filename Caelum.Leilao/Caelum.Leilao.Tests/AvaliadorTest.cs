ca4f7cc23d7ff30023f26d58cc3760f7f4381b0d
larioDiniz <lario.diniz@palo.com.br>
2017-10-20 14:58:15 -0200

    Aula 01

---
+++ Caelum.Leilao/Caelum.Leilao.Tests/AvaliadorTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum.Leilao
+{
+    [TestFixture]
+    public class AvaliadorTest
+    {
+        [Test]
+        public void DeveEntenderLancesEmPrdemCrescente()
+        {
+            // 1a parte: cenario
+            Usuario joao = new Usuario("Joao");
+            Usuario jose = new Usuario("Jose");
+            Usuario maria = new Usuario("Maria");
+
+            Leilao leilao = new Leilao("Playstation 3 Novo");
+
+            leilao.Propoe(new Lance(maria, 250.0));
+            leilao.Propoe(new Lance(joao, 300.0));
+            leilao.Propoe(new Lance(jose, 400.0));            
+
+            // 2a parte: acao
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            // 3a parte: validacao
+            double maiorEsperado = 400;
+            double menorEsperado = 250;
+
+            Assert.AreEqual(maiorEsperado, leiloeiro.MaiorLance);
+            Assert.AreEqual(menorEsperado, leiloeiro.Menorlance);            
+        }
+
+        [Test]
+        public void OutroCenario()
+        {
+            // 1a parte: cenario
+            Usuario joao = new Usuario("Joao");
+            Usuario jose = new Usuario("Jose");
+            Usuario maria = new Usuario("Maria");
+
+            Leilao leilao = new Leilao("Playstation 3 Novo");
+
+            leilao.Propoe(new Lance(maria, 250.0));
+            leilao.Propoe(new Lance(joao, 300.0));
+            leilao.Propoe(new Lance(jose, 400.0));
+
+            // 2a parte: acao
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            // 3a parte: validacao
+            double maiorEsperado = 400;
+            double menorEsperado = 250;
+
+            Assert.AreEqual(maiorEsperado, leiloeiro.MaiorLance);
+            Assert.AreEqual(menorEsperado, leiloeiro.Menorlance);
+        }
+    }
+}
---
+++ Caelum.Leilao/Caelum.Leilao.Tests/PalindromoTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum.Leilao
+{
+    [TestFixture]
+    public class PalindromoTest
+    {
+        [Test]
+        public void DeveIdentificarPalindromoEFiltrarCaracteresInvalidos()
+        {
+            Palindromo p = new Palindromo();
+
+            bool resultado = p.EhPalindromo(
+                "Socorram-me subi no onibus em Marrocos");
+            Assert.IsTrue(resultado);
+        }
+
+        [Test]
+        public void DeveIdentificarPalindromo()
+        {
+            Palindromo p = new Palindromo();
+
+            bool resultado = p.EhPalindromo(
+                "Anotaram a data da maratona");
+            Assert.IsTrue(resultado);
+        }
+
+        [Test]
+        public void DeveIdentificarSeNaoEhPalindromo()
+        {
+            Palindromo p = new Palindromo();
+
+            bool resultado = p.EhPalindromo(
+                "E preciso amar as pessoas como se nao houvesse amanha");
+            Assert.IsFalse(resultado);
+        }
+    }
+}
